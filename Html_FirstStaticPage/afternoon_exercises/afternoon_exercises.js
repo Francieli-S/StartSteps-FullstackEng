e9d9af7d0054868e5301631d93384c4baabef736
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-05-30 18:00:41 +0200

    add task 5

--- Html_FirstStaticPage/afternoon_exercises/afternoon_exercises.js
+++ Html_FirstStaticPage/afternoon_exercises/afternoon_exercises.js
@@ -10,13 +10,13 @@ const calculateSum = (num1, num2) => {
 };
 
 const displaySumResult = (num1, num2) => {
-  result.innerText = calculateSum(num1, num2)
+  result.innerText = calculateSum(num1, num2);
 };
 
 buttonSum.addEventListener('click', () => {
   displaySumResult(inputOne.value, inputTwo.value);
-  inputOne.value = ''
-  inputTwo.value = ''
+  inputOne.value = '';
+  inputTwo.value = '';
 });
 
 // Task 4 Generate random number:
@@ -25,9 +25,31 @@ const buttonGenerate = document.getElementById('button-generate');
 const displayRandomNum = document.getElementById('random-num');
 
 const generateRandomNumber = () => {
-   return (Math.floor(Math.random() * 100) + 1); 
-}
+  return Math.floor(Math.random() * 100) + 1;
+};
 
 buttonGenerate.addEventListener('click', () => {
-  displayRandomNum.innerText = generateRandomNumber()
-})
+  displayRandomNum.innerText = generateRandomNumber();
+});
+
+// Task 5 Change Bg colour
+const buttonDarkMode = document.getElementById('toggle-bg');
+const bodyBg = document.getElementById('color');
+
+let darkMode = false;
+
+const toggleBg = () => {
+  if (!darkMode) {
+    bodyBg.style.backgroundColor = 'black';
+    bodyBg.style.color = 'white';
+    darkMode = true;
+  } else {
+    bodyBg.style.backgroundColor = 'white';
+    bodyBg.style.color = 'black';
+    darkMode = false;
+  }
+};
+
+buttonDarkMode.addEventListener('click', () => {
+  toggleBg();
+});

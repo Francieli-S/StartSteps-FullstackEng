cea677961b9326eddf6b8511b86b0fb15688ef14
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-06-04 17:28:46 +0200

    add tasks 1, 2, and bonus 1 in progress

--- Html_FirstStaticPage/afternoon_3May/afternoon_3May.js
+++ Html_FirstStaticPage/afternoon_3May/afternoon_3May.js
@@ -1,15 +1,65 @@
 // Task 1 Basic counter:
 const btnIncrement = document.getElementById('increment-btn');
+const btnDecrement = document.getElementById('decrement-btn');
 const count = document.getElementById('count');
 
 let counter = 0;
-let darkerGreen = true;
+// let darkerGreen = true;
 
 btnIncrement.addEventListener('click', () => {
-  darkerGreen
-    ? (btnIncrement.style.backgroundColor = 'rgb(112, 224, 200)')
-    : (btnIncrement.style.backgroundColor = 'rgb(12, 173, 138)');
   counter++;
   count.innerText = counter;
-  darkerGreen = false
+});
+
+btnDecrement.addEventListener('click', () => {
+  counter--;
+  count.innerText = counter;
+});
+
+// Task 2 Simple form validation:
+const btnSubmit = document.getElementById('submit-btn');
+const inputName = document.getElementById('input-name');
+const inputEmail = document.getElementById('input-email');
+
+btnSubmit.addEventListener('click', () => {
+  let name = inputName.value;
+  let email = inputEmail.value;
+
+  if (name !== '' && email !== '') {
+    alert(`${name}, your data has been sent.`);
+  }
+});
+
+// Bonus 1 Interactive Image Gallery
+const flowerOne = document.getElementById('flower1');
+const flowerTwo = document.getElementById('flower2');
+const flowerThree = document.getElementById('flower3');
+
+const flowers = document.getAnimations('flowers')
+
+flowerOneClicked = false
+
+flowerOne.addEventListener('click', () => {
+  if (!flowerOneClicked) {
+    flowerOne.style.width = '100px';
+    flowerOneClicked = true
+  } else {
+    flowerOne.style.width = '50px';
+    flowerOneClicked = false
+  }  
+});
+
+flowers.addEventListener('keydow', (event) => {
+  if(event.key === 'ArrowRight') (
+    
+  )
+})
+
+
+flowerTwo.addEventListener('click', () => {
+  flowerTwo.style.width = '100px';
+});
+
+flowerThree.addEventListener('click', () => {
+  flowerThree.style.width = '100px';
 });

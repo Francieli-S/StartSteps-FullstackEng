dd952ff0547f482b9dc79c7503900b4834b1bea3
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-06-06 17:20:28 +0200

    refactor form validation task and some styles minor changes

--- Html_FirstStaticPage/afternoon_3May/afternoon_3May.js
+++ Html_FirstStaticPage/afternoon_3May/afternoon_3May.js
@@ -20,13 +20,44 @@ btnDecrement.addEventListener('click', () => {
 const btnSubmit = document.getElementById('submit-btn');
 const inputName = document.getElementById('input-name');
 const inputEmail = document.getElementById('input-email');
+const statusSubmit = document.querySelector('#submit-status')
 
-btnSubmit.addEventListener('click', () => {
+function allLetters(input) {
+  let letters = /^[a-zA-Z]+$/
+  return input.match(letters) ? true : false
+}
+
+function isEmail(input) {
+  let characters = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return input.match(characters) ? true : false
+}
+
+const userData = {}
+
+btnSubmit.addEventListener('click', (e) => {
+e.preventDefault()
+
   let name = inputName.value;
   let email = inputEmail.value;
 
-  if (name !== '' && email !== '') {
-    alert(`${name}, your data has been sent.`);
+  let validateName = name !== '' ? allLetters(name) : null
+  let validateEmail = email !== '' ? isEmail(email) : null
+
+  if (validateName && validateEmail) {
+    userData.name = name
+    userData.email = email
+    console.log('userData', userData);
+    // call a submit function to post/send this data somewhere
+    statusSubmit.style.color = 'green'
+    statusSubmit.style.display = 'inline'
+
+    inputName.value = ''
+    inputEmail.value = ''
+
+  } else {
+    statusSubmit.innerText = 'Something is wrong!'
+    statusSubmit.style.color = 'red'
+    statusSubmit.style.display = 'inline'
   }
 });
 

1e8f3ce34a9017e1461e7b2408dbc9613c57cc5d
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-06-17 16:53:18 +0200

    adjusting bubble sort task

--- Data Structures and Algorithms/Algorithms/bubble_sort.js
+++ Data Structures and Algorithms/Algorithms/bubble_sort.js
@@ -1,9 +1,5 @@
 // BUBBLE SORT
-const numbers2 = [5, 4, 3, 2, 1];
-const numbers3 = [10, 9, 8, 7, 6];
-
-function bubbleSort(arr) {
-  let iterations = 0;
+function bubbleSortAscending(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] > arr[j]) {
@@ -11,18 +7,52 @@ function bubbleSort(arr) {
         arr[i] = arr[j];
         arr[j] = temp;
       }
-      iterations++;
-      console.log(`iteration ${i + 1}: `, arr);
     }
   }
-  console.log('iterations nested for loops: ', iterations);
   return arr;
 }
 
-// console.log(bubbleSort(numbers2));
+console.log('bubble sort ascending: ', bubbleSortAscending([10, 7, 6, 3, 1, 2]));
+
+function bubbleSortDescending(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log('bubble sort descending: ', bubbleSortDescending([11, 10, 9, 23, 21]));
+
+
+function bubbleSort(arr, ascending = true) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (ascending) {
+        arr[i] < arr[j];
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      } else {
+        arr[i] > arr[j];
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log('double fn ascending: ', bubbleSort([3, 2, 1]));
+console.log('double fn descending: ', bubbleSort([1, 2, 3], false));
 
 function bubbleSort2(arr) {
-  let iterations = 0;
   let swapped;
   do {
     swapped = false;
@@ -32,13 +62,11 @@ function bubbleSort2(arr) {
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
         swapped = true;
-        iterations++;
-        console.log(`iteration ${i + 1}: `, arr);
       }
     }
   } while (swapped);
-  console.log('iterations with swapped variable: ', iterations);
   return arr;
 }
 
-// console.log(bubbleSort2(numbers3));
+console.log('bubble sort with swapped variable: ', bubbleSort2([10, 7, 6, 3, 9]));
+

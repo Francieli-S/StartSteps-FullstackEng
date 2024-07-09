1f7721222093fbee554180261cfe78fe2b70de92
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-07-09 17:22:22 +0200

    add calculate function and example usage

--- TS_Frontend_OOP/tasksDayTwo.ts
+++ TS_Frontend_OOP/tasksDayTwo.ts
@@ -16,43 +16,73 @@ type Student = {
   //isStuding = (): void => {}
   grade: number;
   uniqueSymbol: symbol;
+  // from task 5:
+  allScores: number[];
 };
 
 // Task 3
-var varVariable = false
+var varVariable = false;
 // variable = `A variable declared without key word uses 'var'`
 
 // console.log(variable);
 
 const isGlobalScope = (): void => {
-  let letVariable = false
-  varVariable = true
+  let letVariable = false;
+  varVariable = true;
 
   console.log('Is global?: ', 'let: ', letVariable);
   console.log('Is global?: ', 'var: ', varVariable);
-}
+};
 
 isGlobalScope();
 
 function isLocalScope(): void {
-  let letVariable2 = false
-  var varVariable2 = true
-  
+  let letVariable2 = false;
+  var varVariable2 = true;
+
   console.log('Variables letVariable2 and varVariable2 are local scope');
 }
 
 // if (varVariable2) {
-  //   console.log('is global');
-  // } else {
-    //   console.log('is local');
-    // }
-    
-isLocalScope()
+//   console.log('is global');
+// } else {
+//   console.log('is local');
+// }
+
+isLocalScope();
 
 // Task 4
-// I'm not doing it because I have doing it for some weeks.
+// I'm not doing it because I have been doing it for some weeks.
 
 // Task 5
+const calculateGrade = (student: Student): string | undefined => {
+  let averageScore: number = +(
+    student.allScores.reduce((sum, score) => sum + score, 0) / student.allScores.length
+  ).toFixed(2);
+  console.log(averageScore);
 
+  if (averageScore >= 90) {
+    return 'Grade A';
+  } else if (averageScore >= 80) {
+    return 'Grade B';
+  } else if (averageScore >= 70) {
+    return 'Grade C';
+  } else if (averageScore >= 60) {
+    return 'Grade D';
+  } else if (averageScore < 60) {
+    return 'Grade F';
+  }
+};
 
-    
+const studentOne: Student = {
+  studentName: 'Bento',
+  studentId: 1,
+  subject: ['Math', 'Hunt', 'Cuddle'],
+  major: false,
+  grade: 11,
+  uniqueSymbol: Symbol('B'),
+  allScores: [100, 92.55, 8, 97],
+};
+
+const studentOneGrade = calculateGrade(studentOne);
+console.log(studentOneGrade);

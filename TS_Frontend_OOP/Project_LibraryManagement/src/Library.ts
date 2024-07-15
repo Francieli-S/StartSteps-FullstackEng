237a9406a6e89262f18e1db35402e3f6a908d5ce
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-07-15 15:56:42 +0200

    add class Library

---
+++ TS_Frontend_OOP/Project_LibraryManagement/src/Library.ts
@@ -0,0 +1,34 @@
+// Is it because Library interface will not be imported from the other files that we creacre it inside the Library file?
+
+import { Book } from './Book';
+import { BookDetails, BookGenre } from './Types';
+
+interface LibraryDetails {
+  name: string;
+  address: string;
+}
+
+export class Library {
+  name: string;
+  address: string;
+  books: Book[];
+
+  constructor(libray: LibraryDetails) {
+    this.name = libray.name;
+    this.address = libray.address;
+    this.books = [];
+  }
+
+  addBook = (book: BookDetails): void => {
+    const newBook = new Book(book);
+    this.books.push(newBook);
+  };
+
+  removeBook = (title: string): void => {
+    this.books = this.books.filter((book) => book.title !== title);
+  };
+
+  getBooks = (): Book[] => {
+    return this.books;
+  };
+}

876412533c804cbc533c5b417d4565e60eb8861a
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-07-15 14:36:06 +0200

    create an instance of Book and add some coments/questions

--- TS_Frontend_OOP/Project_LibraryManagement/src/Book.ts
+++ TS_Frontend_OOP/Project_LibraryManagement/src/Book.ts
@@ -6,13 +6,14 @@ class Book {
   private _publishedYear: number
   private _genre : BookGenre 
 
-  constructor(book: BookDetails) {
+  constructor(book: BookDetails) { //how to set default parameters?
     this._title = book.title
     this._author = book.author
     this._publishedYear = book.publishedYear
     this._genre = book.genre
   }
-
+  
+  // code some validation to the getters
   get title(): string {
     return this._title
   }
@@ -45,4 +46,24 @@ class Book {
     this._genre = genre
   }
 
+  // Is it okay to use arrow function here but not with the getters and setters?
+  // IMPLEMENTING LIBRARY FIRST
+  // getBookDetails = (title: string): BookDetails => {
+
+  // } 
+
 }
+
+// I'm not sure if this is the right way to do it. It seems I am repeating
+// If it is right, how to name this object and the instance?
+const bookOne: BookDetails = {
+  title: 'Cat Kingdom',
+  author: 'Franci',
+  publishedYear: 2024,
+  genre: BookGenre.SCIENCE
+}
+
+const bookOne1 = new Book(bookOne)
+let {title, author, publishedYear, genre} = bookOne1
+console.log(bookOne1);
+console.log(`Title: ${title}, author: ${author}, publishedYear: ${publishedYear}, genre: ${genre}.`);

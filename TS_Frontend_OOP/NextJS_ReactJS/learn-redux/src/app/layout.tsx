af4b4bdd4a6528d1661a320e284e5ecdb9153e03
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-08-07 14:29:14 +0200

    implement store that contains the global state, the reducer function, and use it in the layout

--- TS_Frontend_OOP/NextJS_ReactJS/learn-redux/src/app/layout.tsx
+++ TS_Frontend_OOP/NextJS_ReactJS/learn-redux/src/app/layout.tsx
@@ -1,8 +1,5 @@
+import StoreWrapper from "@/store/store";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <StoreWrapper>{children}</StoreWrapper>
+      </body>
     </html>
   );
 }

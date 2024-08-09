7f36b2f1b75ae6c0d2804718615ac5668878a650
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-08-09 10:28:40 +0200

    update with comments regarding to use async/await

--- TS_Frontend_OOP/NextJS_ReactJS/next-api-routes/src/app/fetching_example/page.tsx
+++ TS_Frontend_OOP/NextJS_ReactJS/next-api-routes/src/app/fetching_example/page.tsx
@@ -11,51 +11,56 @@ interface ITodo {
 const FetchComponent: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[] | null>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  // const [error, setError] = useState<Error | null>();
+  const [error, setError] = useState<Error | null>();
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-      if (response.status === 200) {
-        const data = await response.json()
-        setLoading(false)
-        setTodos(data)
-      }
-    } catch (error) {
-      setLoading(false);
-      // setError(error); // in this example, the error setting gives me a warn.
-    }
-  }
-  useEffect(() => {
-    fetchData()
-  }, []);
-
+  // using async/await is not possible in the client side; it worked here, maybe, because I called the
+  // function inside the useEffect. But it is also not a good practice await for the data in the UI, otherwise the code 
+  // will not hit the 'Loading...' part.
+  // 
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+  //     if (response.status === 200) {
+  //       const data = await response.json()
+  //       setLoading(false)
+  //       setTodos(data)
+  //     }
+  //   } catch (error) {
+  //     setLoading(false);
+  //     // setError(error); // in this example, the error setting gives me a warn.
+  //   }
+  // }
   // useEffect(() => {
-  //   fetch('https://jsonplaceholder.typicode.com/todos')
-  //     .then((response) => {
-  //       // handle the response here
-  //       console.log(response);
-  //       if (response.status === 200) return response.json();
-  //     })
-  //     .then((data: ITodo[]) => {
-  //       console.log(data);
-  //       setLoading(false);
-  //       setTodos(data);
-  //     })
-  //     .catch((error: Error) => {
-  //       // if no response or error in the request, handle the error
-  //       console.log(error);
-  //       setLoading(false);
-  //       setError(error);
-  //     });
+  //   fetchData()
   // }, []);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => { // resolved
+        // handle the response here
+        console.log(response);
+        return response.json();
+      })
+      .then((data: ITodo[]) => { // resolved
+        // got the data succefully
+        console.log(data);
+        setLoading(false);
+        setTodos(data);
+      })
+      .catch((error: Error) => { // rejected
+        // if no response or error in the request, handle the error
+        console.log(error);
+        setLoading(false);
+        setError(error);
+      });
+  }, []);
 
   if (loading) {
     return <p>Loading...</p>;
   }
-  // if (error) {
-  //   return <p>An error happened: {error.message}</p>;
-  // }
+  if (error) {
+    return <p>An error happened: {error.message}</p>;
+  }
   return (
     // <p>{JSON.stringify(todos, null, 2)}</p>
     todos && todos.map((todo) => (

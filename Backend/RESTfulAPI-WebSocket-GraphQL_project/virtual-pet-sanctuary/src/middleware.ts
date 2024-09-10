74eaa0c981660af3ec97e41e198a462e3f94306f
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-09-10 11:33:50 +0200

    add controller, data handlers, middlewares, and routes

---
+++ Backend/RESTfulAPI-WebSocket-GraphQL_project/virtual-pet-sanctuary/src/middleware.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+
+// the name of the middleware should be the same as the controller
+
+const createPet = (req: Request, res: Response, next: Function) => {
+  const { name, species } = req.body;
+  if (!name || !species) {
+    res.status(400).send('Name and species are required');
+    return;
+  }
+// add validation to not accept props that are not part of the type, like age
+  // if ()
+  next();
+};
+
+const updatePetHappiness = (req: Request, resp: Response, next: Function) => {
+  if (!req.body.happiness) {
+    resp.status(400).send('Happiness is required');
+    return;
+  }
+  next();
+};
+
+const auth = (req: Request, resp: Response, next: Function) => {
+  // authentication using bearer token
+  if (req.headers.authorization !== 'Bearer password123') {
+    resp.status(401).send('Unauthorized');
+    return;
+  }
+  next();
+};
+
+export default {
+  createPet,
+  updatePetHappiness,
+  auth,
+};
---
+++ Backend/RESTfulAPI-WebSocket-GraphQL_project/virtual-pet-sanctuary/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import petController from "./controller.js";
+import petMiddleware from './middleware.js'
+
+const router = Router()
+
+//RESTful API routes and handelers
+
+router.get("/", petController.getPets);
+router.get('/:id',petController.getPet);
+router.post('/', petMiddleware.auth, petMiddleware.createPet, petController.createPet); 
+router.patch('/:id', petMiddleware.auth, petMiddleware.updatePetHappiness, petController.updatePetHappiness);
+router.delete('/:id', petMiddleware.auth, petController.deletePet);
+
+export default router;

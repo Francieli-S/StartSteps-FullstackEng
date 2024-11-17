b1c299cdbb66961a1ef5a052426fd1e0158b129d
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-11-17 13:43:34 +0100

    add another example to the Dockerfile and add .dockerignore

--- Docker/my-ts-app/index.ts
+++ Docker/my-ts-app/index.ts
@@ -1,9 +1,27 @@
-import express from 'express'
+// src/math.ts
+export class MathOperations {
+  // using static allows to use the method without create a new instance
+  static add(a: number, b: number): number {
+    return a + b;
+  }
 
-const app = express()
-app.use(express.json())
+  static multiply(a: number, b: number): number {
+    return a * b;
+  }
+}
 
-app.listen(3000, () => {
-  console.log('Server is running at port 3000');
-})
- 
+// Example usage
+const resultAdd = MathOperations.add(5, 3);
+console.log(`Addition Result: ${resultAdd}`);
+
+const resultMultiply = MathOperations.multiply(5, 3);
+console.log(`Multiply Result: ${resultMultiply}`);
+
+// import express from 'express'
+
+// const app = express()
+// app.use(express.json())
+
+// app.listen(3000, () => {
+//   console.log('Server is running at port 3000');
+// })
